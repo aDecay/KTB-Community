9475d65fd02c373bdba7c221d4246b837edad98e
adecay <younghyeon55@gmail.com>
2026-06-09 17:30:40 +0900

    feat: add cors settings

--- src/main/java/com/chad/community/config/WebConfig.java
+++ src/main/java/com/chad/community/config/WebConfig.java
@@ -2,19 +2,38 @@ package com.chad.community.config;
 
 import com.chad.community.resolver.AuthenticationArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final AuthenticationArgumentResolver authenticationArgumentResolver;
+    private final String[] allowedOrigins;
+
+    public WebConfig(
+            AuthenticationArgumentResolver authenticationArgumentResolver,
+            @Value("${cors.allowed-origins}") String[] allowedOrigins
+    ) {
+        this.authenticationArgumentResolver = authenticationArgumentResolver;
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
